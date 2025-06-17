8ed9a1f59118ec64885d2e5f497ba8ea53f9a53a
James Patrick Bermoy <jphabsart@gmail.com>
2025-06-17 17:31:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/movie/movie-list.tsx
+++ src/components/movie/movie-list.tsx
@@ -110,9 +110,10 @@ export default function MovieList({ movies, loading = false, itemsPerPage = DEFA
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+      {movies.map((movie, index) => (
+        <MovieCard key={`${movie.id}-${index}`} movie={movie} />
       ))}
     </div>
   );
 }
+
